bc9826e3a80583200a5d638909fc6eff4d7f3049
dnavmoy <dnavmoy119@g.educaand.es>
2023-12-01 09:34:00 +0100

    creo las clases y enum

--- src/main/java/daw/main.java
+++ src/main/java/daw/main.java
@@ -10,6 +10,22 @@ package daw;
  */
 public class main {
 
+//    nuevo proyecto
+//clase naipe - dos atributos: numero carta y palo
+//1-7 8 sota 9 caballo 10 rey
+//palos oros bastos espadas y copas
+//   dos construtores 1 crea cartas aleatoriamente -> por defecto
+//   2 parametros- numero de carta y palo si no es correcto lanzar excepcion
+//   
+//clase baraja
+//	atributo array de naipes 
+//	constructor por defecto que añade todos los naipes de la baraja
+//	
+//	
+//main para crear naipes sueltos
+//	y crear baraja
+//    
+    
     public static void main(String[] args) {
        
     }
