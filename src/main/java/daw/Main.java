947db0fb2ba75a0cdae88d25a0d9a09e02f06b49
Daniel <dnavmoy119@g.educaand.es>
2023-12-06 15:52:57 +0000

    terminado. revisar y poner bonito si eso

--- src/main/java/daw/Main.java
+++ src/main/java/daw/Main.java
@@ -3,67 +3,45 @@
  */
 package daw;
 
-
 /**
  *
  * @author daniel
  */
 public class Main {
 
-//    nuevo proyecto
-//clase baraja
-//	atributo array de naipes 
-//	constructor por defecto que añade todos los naipes de la baraja
-//	
-//	
-//main para crear naipes sueltos
-//	y crear baraja y mostrar todas las cartas  
-//      sacar cartas aleatoriamente hata que salga la carta que se solicita al ususario
-    
-    //prueba git 
-    //prueba git 2
     public static void main(String[] args) {
-            
-        Naipe carta;
-        Naipe ReyOros= new Naipe(10, Palos.Oro);
-        
-         
-        
-          do{
-              carta = new Naipe();
-              System.out.println(carta);
-          }while(!((carta.getNumero()==ReyOros.getNumero())&&carta.getPalo()==ReyOros.getPalo()));
-        
-//        Naipe[] pruebaArray = new Naipe[50];
-//
-//        for (int i = 0; i < pruebaArray.length; i++) {
-//            pruebaArray[i] = new Naipe();
-//
-//        }
-//        for (int i = 0; i < pruebaArray.length; i++) {
-//            System.out.println(pruebaArray[i]);
-//
-//        }
-//     
-//        try{
-//        Naipe pruebaNaipe = new Naipe( 7, Palos.Oro);
-//        System.out.println("prueba naipe es : " + pruebaNaipe);
-//        }catch (IllegalArgumentException IAE){
-//            System.out.println(IAE.getMessage());
-//        }
-//        
-//       Ba
-           
 
-        
+        Naipe carta;
+        Naipe ReyOros = new Naipe(10, Palos.Oro);
+
+        do {
+            carta = new Naipe();
+            System.out.println(carta);
+        } while (!((carta.getNumero() == ReyOros.getNumero()) && carta.getPalo() == ReyOros.getPalo()));
+        System.out.println("ha mostrado el rey de oros?\n");
+
         System.out.println("Mostrar una baraja entera: ");
-     
+
         parteB.Baraja pruebaBaraja = new parteB.Baraja();
+
+        System.out.println(pruebaBaraja);
+        System.out.println("\n barajo 50 cartas");
+
+        pruebaBaraja.barajar(50);
+        System.out.println(pruebaBaraja);
+
+        int sacarCartas = 15;
         
-         System.out.println(pruebaBaraja);
-        
-        pruebaBaraja.barajar(15);
+        System.out.println("saco " + sacarCartas + " cartas");
+        Naipe[] cartasSacadas = new Naipe[sacarCartas];
+        cartasSacadas = pruebaBaraja.sacar(sacarCartas);
+        System.out.println("como queda la baraja: ");
         System.out.println(pruebaBaraja);
         
+        System.out.println("\n las cartas sacadas:");
+        for (int i = 0; i < cartasSacadas.length; i++) {
+            System.out.println(cartasSacadas[i]);
+        }
+
     }
 }
--- src/main/java/parteB/Baraja.java
+++ src/main/java/parteB/Baraja.java
@@ -46,8 +46,10 @@ public class Baraja {
     public String toString() {
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < array.length; i++) {
-            sb.append("naipe{");
-            if (array[i].getNumero() == 8) {
+            sb.append("\n naipe{");
+            if (array[i] == null) {
+                sb.append("numero=").append(" vacio");
+            } else if (array[i].getNumero() == 8) {
                 sb.append("numero=").append("sota");
             } else if (array[i].getNumero() == 9) {
                 sb.append("numero=").append("caballo");
@@ -57,25 +59,51 @@ public class Baraja {
                 sb.append("numero=").append(array[i].getNumero());
 
             }
-
-            sb.append(", palo=").append(array[i].getPalo());
+            if (array[i] == null) {
+                sb.append(", palo=").append(" null");
+            } else {
+                sb.append(", palo=").append(array[i].getPalo());
+            }
             sb.append('}');
-            System.out.println("");
+
         }
+        System.out.println("");
         return sb.toString();
     }
 
-    public  void barajar(int movimientos) {
+    public void barajar(int movimientos) {
 
         for (int i = 0; i < movimientos; i++) {
             Random r = new Random();
-            int cartaAMover = r.nextInt(39);
-            int otraCarta = r.nextInt(39);
-            Naipe cartaTemporal= new Naipe(array[cartaAMover].getNumero(), array[cartaAMover].getPalo());
+            int cartaAMover = r.nextInt(40);
+            int otraCarta = r.nextInt(40);
+            Naipe cartaTemporal = new Naipe(array[cartaAMover].getNumero(), array[cartaAMover].getPalo());
             array[cartaAMover].setNumero(array[otraCarta].getNumero());
             array[cartaAMover].setPalo(array[otraCarta].getPalo());
             array[otraCarta].setNumero(cartaTemporal.getNumero());
             array[otraCarta].setPalo(cartaTemporal.getPalo());
+        }
+
+    }
+
+    public Naipe[] sacar(int numCartas) {
+
+        Random r = new Random();
+        if (numCartas < 1 || numCartas > 40) {
+            daw.Naipe[] cartasSacadas = new daw.Naipe[0];
+            return cartasSacadas;
+        } else {
+            daw.Naipe[] cartasSacadas = new daw.Naipe[numCartas];
+            for (int i = 0; i < cartasSacadas.length; i++) {
+                int cartaASacar = r.nextInt(40);
+                if (array[cartaASacar] == null) {
+                    i--;
+                } else {
+                    cartasSacadas[i] = new Naipe(array[cartaASacar].getNumero(), array[cartaASacar].getPalo());
+                    array[cartaASacar] = null;
+                }
+            }
+            return cartasSacadas;
         }
 
     }
